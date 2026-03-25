c488eead6073b413a1667203056d60f8cf03b771
luniana217 <hehe627@gmail.com>
2026-03-25 10:03:57 +0900

    색 오류 수정 완료

--- frontend/src/pages/Card.jsx
+++ frontend/src/pages/Card.jsx
@@ -18,22 +18,22 @@ import 'react-tooltip/dist/react-tooltip.css';
 import '../assets/ScatterLegend.css'; 
 
 const INSIGHTS = {
-  focus: {
+  Efocus: {
     title: "업무 중심지",
     description: "강남, 여의도, 시청 같은 전형적인 중심 업무 지구입니다.",
     
   },
-  dominantEvening: {
+  Edominant: {
     title: "퇴근 유입지 / 주거 밀집지",
     description: `흔히 말하는 '베드타운'보다는, 주거지 근처에서 오후~저녁에 경제 활동을 시작하는 인구가 많은 지역일 수 있습니다.`
     
   },
-  dominantMorning: {
+  Mdominant: {
     title: "출근 유입지",
     description: "이곳은 직장인들이 출근은 하지만, 퇴근 시에는 다른 곳(근처 핫플레이스나 회식 장소)으로 이동하여 지하철을 탈 확률이 높습니다."
     
   },
-  normal: {
+  Nnormal: {
     title: "일반 / 저유동",
     description: "직장인 타겟 마케팅에서는 비용 효율을 위해 우선순위에서 배제해야 할 지역입니다."
    
@@ -338,13 +338,13 @@ const Card = () => {
               </div>
 
               <div className="type-tag">
-                <span className="dot dominantEvening" />
+                <span className="dot mixed" />
                 <span className="type-name">우세 지역</span>
                 <span className="val-sub">비중 격차 5~49%</span>
               </div>
 
               <div className="type-tag">
-                <span className="dot dominantMorning" />
+                <span className="dot Eddominant" />
                 <span className="type-name">복합 지역</span>
                 <span className="val-sub">비중 격차 5%↓</span>
               </div>
